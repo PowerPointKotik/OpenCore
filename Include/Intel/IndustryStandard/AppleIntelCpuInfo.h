e5c9fed226d95896a384556922f1fce43cab8396
overlordkdone <overlordkdone@github.com>
2026-07-12 17:49:14 +1200

    Fix MicroTool: add missing EFI_AP_PROCEDURE typedefs to AppleIntelCpuInfo.h

--- Include/Intel/IndustryStandard/AppleIntelCpuInfo.h
+++ Include/Intel/IndustryStandard/AppleIntelCpuInfo.h
@@ -5,4 +5,10 @@
   Redirect from Intel/IndustryStandard to Apple/IndustryStandard path.
 **/
 
+#ifndef EFI_AP_PROCEDURE
+typedef VOID (EFIAPI *EFI_AP_PROCEDURE)(IN OUT VOID *Buffer);
+#endif
+typedef EFI_AP_PROCEDURE EFI_MP_SERVICES_STARTUP_ALL_APS;
+typedef EFI_AP_PROCEDURE EFI_MP_SERVICES_STARTUP_THIS_AP;
+
 #include <Apple/IndustryStandard/AppleIntelCpuInfo.h>
